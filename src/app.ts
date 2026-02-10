94f72177d13727554aaa94a6725be4ceb03b9fab
Moises Velasquez <moises007@gmail.com>
2026-02-09 23:35:39 -0500

    chore(app): add BroadCast, all clients display messages

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 
 const wss = new WebSocketServer({ port: 6000 });
 
@@ -8,9 +8,19 @@ wss.on('connection', function connection(ws) {
 
   ws.on('message', function message(data) {
     console.log('received: %s', data);
+    const payload = JSON.stringify({
+      type:'custom-message',
+      payload: data.toString(),
+    })
+    // ws.send(JSON.stringify(payload))
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(payload, { binary: false });
+      }
+    });
   });
 
-  ws.send('Hello from server!');
+  // ws.send('Hello from server!');
 
   ws.on('close', ()=>{
     console.log('Client disconnected')
